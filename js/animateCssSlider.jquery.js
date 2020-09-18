649f87efc9521a5b4bd036bf32ddeffaf3fecdc6
Anton Chyvikov <chivikovanton@gmail.com>
2020-09-18 17:48:24 +0300

    15 animation

--- js/animateCssSlider.jquery.js
+++ js/animateCssSlider.jquery.js
@@ -9,11 +9,26 @@
             animationType: 'fadeOut/fadeIn',
             animationTime: '',
             constSliderHeight: false,
+            overflowHidden: true
         }
 
         // animationType:
         // fadeOut/fadeIn
-        // fadeDown/fadeUp
+        // fadeOutInDown/fadeOutInUp
+        // fadeOutInDownBig/fadeOutInUpBig
+        // fadeOutInLeft/fadeOutInRight
+        // fadeOutInLeftBig/fadeOutInRightBig
+        // fadeOutInLeftTop/fadeOutInRightBottom
+        // fadeOutInLeftBottom/fadeOutInRightTop
+        // backOutInDown/backOutInUp
+        // backOutInLeft/backOutInRight
+        // bounceOut/bounceIn
+        // bounceOutInDown/bounceOutInUp
+        // bounceOutInLeft/bounceOutInRight
+        // flipOutX/flipInX
+        // flipOutY/flipInY
+        // lightSpeedOutInLeft/lightSpeedOutInRight
+        // rotateOut/rotateIn
 
         // animationTime:
         // animate__slow	2s
@@ -51,6 +66,7 @@
         let counter = 0
         let slideshowInterval
         let startSlidShowInterval
+        let slideShowFlag = null
 
         //Disable button during animation
         let isRun = false
@@ -76,7 +92,30 @@
             }
         })
 
-        startSlideShow()
+        $(window).scroll(function () {
+            var wt = $(window).scrollTop();
+            var wh = $(window).height();
+            var et = $container.offset().top;
+            var eh = $container.outerHeight();
+
+            if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)) {
+                if (slideShowFlag == null || slideShowFlag == false) {
+                    let timer = setTimeout(() => {
+                        next()
+                        clearTimeout(timer)
+                    }, 1000)
+                    startSlideShow()
+                }
+                slideShowFlag = true;
+            } else {
+                if (slideShowFlag == null || slideShowFlag == true) {
+                    clearInterval(slideshowInterval)
+                    clearInterval(startSlidShowInterval)
+                }
+                slideShowFlag = false;
+            }
+        });
+
 
         $btnNext.on('click', () => {
             clearInterval(slideshowInterval)
@@ -116,7 +155,7 @@
                     resizeContainer()
                 })
             } else {
-                    $container.css('height', `${getMaxSlideHeight() + $btnGroup.outerHeight() * 2}px`)
+                $container.css('height', `${getMaxSlideHeight() + $btnGroup.outerHeight() * 2}px`)
             }
         })
 
@@ -142,7 +181,11 @@
         }
 
         function addStyles() {
-            $container.css('position', 'relative').css('overflow', 'hidden').css('transition', 'all linear 0.3s')
+            $('body').css('overflow-x', 'hidden')
+            $container.css('position', 'relative').css('transition', 'all linear 0.3s')
+            if (options.overflowHidden === true) {
+                $container.css('overflow', 'hidden')
+            }
             $wrappers.css('position', 'relative')
             $images.css('display', 'block').css('position', 'absolute').css('opacity', '0').css('top', '0')
             $images.eq(counter).css('position', 'static').css('opacity', '1').css('margin', '0 auto')
@@ -189,9 +232,37 @@
         function getNextAnimationFunction() {
             switch (true) {
                 case (options.animationType === 'fadeOut/fadeIn'):
-                    return nextSlideFadeOutFadeIn()
-                case (options.animationType === 'fadeDown/fadeUp'):
-                    return nextSlideFadeDownFadeUp()
+                    return sliderAnimation('animate__fadeOut', counterInc, 'animate__fadeIn')
+                case (options.animationType === 'fadeOutInDown/fadeOutInUp'):
+                    return sliderAnimation('animate__fadeOutDown', counterInc, 'animate__fadeInDown')
+                case (options.animationType === 'fadeOutInDownBig/fadeOutInUpBig'):
+                    return sliderAnimation('animate__fadeOutDownBig', counterInc, 'animate__fadeInDownBig')
+                case (options.animationType === 'fadeOutInLeft/fadeOutInRight'):
+                    return sliderAnimation('animate__fadeOutLeft', counterInc, 'animate__fadeInRight')
+                case (options.animationType === 'fadeOutInLeftBig/fadeOutInRightBig'):
+                    return sliderAnimation('animate__fadeOutLeftBig', counterInc, 'animate__fadeInRightBig')
+                case (options.animationType === 'fadeOutInLeftTop/fadeOutInRightBottom'):
+                    return sliderAnimation('animate__fadeOutTopLeft', counterInc, 'animate__fadeInBottomRight')
+                case (options.animationType === 'fadeOutInLeftBottom/fadeOutInRightTop'):
+                    return sliderAnimation('animate__fadeOutBottomLeft', counterInc, 'animate__fadeInTopRight')
+                case (options.animationType === 'backOutInDown/backOutInUp'):
+                    return sliderAnimation('animate__backOutDown', counterInc, 'animate__backInDown')
+                case (options.animationType === 'backOutInLeft/backOutInRight'):
+                    return sliderAnimation('animate__backOutLeft', counterInc, 'animate__backInRight')
+                case (options.animationType === 'bounceOut/bounceIn'):
+                    return sliderAnimation('animate__bounceOut', counterInc, 'animate__bounceIn')
+                case (options.animationType === 'bounceOutInDown/bounceOutInUp'):
+                    return sliderAnimation('animate__bounceOutDown', counterInc, 'animate__bounceInDown')
+                case (options.animationType === 'bounceOutInLeft/bounceOutInRight'):
+                    return sliderAnimation('animate__bounceOutLeft', counterInc, 'animate__bounceInRight')
+                case (options.animationType === 'flipOutX/flipInX'):
+                    return sliderAnimation('animate__flipOutX', counterInc, 'animate__flipInX')
+                case (options.animationType === 'flipOutY/flipInY'):
+                    return sliderAnimation('animate__flipOutY', counterInc, 'animate__flipInY')
+                case (options.animationType === 'lightSpeedOutInLeft/lightSpeedOutInRight'):
+                    return sliderAnimation('animate__lightSpeedOutLeft', counterInc, 'animate__lightSpeedInRight')
+                case (options.animationType === 'rotateOut/rotateIn'):
+                    return sliderAnimation('animate__rotateOut', counterInc, 'animate__rotateIn')
 
             }
         }
@@ -199,9 +270,37 @@
         function getPrevAnimationFunction() {
             switch (true) {
                 case (options.animationType === 'fadeOut/fadeIn'):
-                    return prevSlideFadeOutFadeIn()
-                case (options.animationType === 'fadeDown/fadeUp'):
-                    return prevSlideFadeDownFadeUp()
+                    return sliderAnimation('animate__fadeOut', counterDec, 'animate__fadeIn')
+                case (options.animationType === 'fadeOutInDown/fadeOutInUp'):
+                    return sliderAnimation('animate__fadeOutUp', counterDec, 'animate__fadeInUp')
+                case (options.animationType === 'fadeOutInDownBig/fadeOutInUpBig'):
+                    return sliderAnimation('animate__fadeOutUpBig', counterDec, 'animate__fadeInUpBig')
+                case (options.animationType === 'fadeOutInLeft/fadeOutInRight'):
+                    return sliderAnimation('animate__fadeOutRight', counterDec, 'animate__fadeInLeft')
+                case (options.animationType === 'fadeOutInLeftBig/fadeOutInRightBig'):
+                    return sliderAnimation('animate__fadeOutRightBig', counterDec, 'animate__fadeInLeftBig')
+                case (options.animationType === 'fadeOutInLeftTop/fadeOutInRightBottom'):
+                    return sliderAnimation('animate__fadeOutBottomRight', counterDec, 'animate__fadeInTopLeft')
+                case (options.animationType === 'fadeOutInLeftBottom/fadeOutInRightTop'):
+                    return sliderAnimation('animate__fadeOutTopRight', counterDec, 'animate__fadeInBottomLeft')
+                case (options.animationType === 'backOutInDown/backOutInUp'):
+                    return sliderAnimation('animate__backOutUp', counterDec, 'animate__backInUp')
+                case (options.animationType === 'backOutInLeft/backOutInRight'):
+                    return sliderAnimation('animate__backOutRight', counterDec, 'animate__backInLeft')
+                case (options.animationType === 'bounceOut/bounceIn'):
+                    return sliderAnimation('animate__bounceOut', counterDec, 'animate__bounceIn')
+                case (options.animationType === 'bounceOutInDown/bounceOutInUp'):
+                    return sliderAnimation('animate__bounceOutUp', counterDec, 'animate__bounceInUp')
+                case (options.animationType === 'bounceOutInLeft/bounceOutInRight'):
+                    return sliderAnimation('animate__bounceOutRight', counterDec, 'animate__bounceInLeft')
+                case (options.animationType === 'flipOutX/flipInX'):
+                    return sliderAnimation('animate__flipOutX', counterDec, 'animate__flipInX')
+                case (options.animationType === 'flipOutY/flipInY'):
+                    return sliderAnimation('animate__flipOutY', counterDec, 'animate__flipInY')
+                case (options.animationType === 'lightSpeedOutInLeft/lightSpeedOutInRight'):
+                    return sliderAnimation('animate__lightSpeedOutRight', counterDec, 'animate__lightSpeedInLeft')
+                case (options.animationType === 'rotateOut/rotateIn'):
+                    return sliderAnimation('animate__rotateOut', counterDec, 'animate__rotateIn')
             }
         }
 
@@ -226,86 +325,26 @@
         }
 
 
-        //========  Functions of animation  ===========
+        //========  Function of animation  ===========
 
-        //FadeOutFadeIn
-        function nextSlideFadeOutFadeIn() {
-            $images.eq(counter).addClass(`animate__animated animate__fadeOut ${options.animationTime}`)
+        function sliderAnimation(animationCurrentClass, counterFunc, animationNextClass) {
+            $images.eq(counter).addClass(`animate__animated ${animationCurrentClass} ${options.animationTime}`)
 
             $images.eq(counter).one('animationend', function () {
-                $images.eq(counter).removeClass(`animate__animated animate__fadeOut ${options.animationTime}`)
+                $images.eq(counter).removeClass(`animate__animated ${animationCurrentClass} ${options.animationTime}`)
                 hideCurrentSlide()
-                counterInc()
+                counterFunc()
                 showCurrentSlide()
                 if (options.constSliderHeight === false) {
                     resizeContainer()
                 }
-                $images.eq(counter).addClass(`animate__animated animate__fadeIn ${options.animationTime}`)
+                $images.eq(counter).addClass(`animate__animated ${animationNextClass} ${options.animationTime}`)
                 $images.eq(counter).one('animationend', function () {
-                    $images.eq(counter).removeClass(`animate__animated animate__fadeIn ${options.animationTime}`)
+                    $images.eq(counter).removeClass(`animate__animated ${animationNextClass} ${options.animationTime}`)
                     isRun = false
                 })
             })
         }
-
-        function prevSlideFadeOutFadeIn() {
-            $images.eq(counter).addClass(`animate__animated animate__fadeOut ${options.animationTime}`)
-
-            $images.eq(counter).one('animationend', function () {
-                $images.eq(counter).removeClass(`animate__animated animate__fadeOut ${options.animationTime}`)
-                hideCurrentSlide()
-                counterDec()
-                showCurrentSlide()
-                if (options.constSliderHeight === false) {
-                    resizeContainer()
-                }
-                $images.eq(counter).addClass(`animate__animated animate__fadeIn ${options.animationTime}`)
-                $images.eq(counter).one('animationend', function () {
-                    $images.eq(counter).removeClass(`animate__animated animate__fadeIn ${options.animationTime}`)
-                    isRun = false
-                })
-            })
-        }
-
-        //FadeDownFadeUp
-        function nextSlideFadeDownFadeUp() {
-            $images.eq(counter).addClass(`animate__animated animate__fadeOutDown ${options.animationTime}`)
-
-            $images.eq(counter).one('animationend', function () {
-                $images.eq(counter).removeClass(`animate__animated animate__fadeOutDown ${options.animationTime}`)
-                hideCurrentSlide()
-                counterInc()
-                showCurrentSlide()
-                if (options.constSliderHeight === false) {
-                    resizeContainer()
-                }
-                $images.eq(counter).addClass(`animate__animated animate__fadeInDown ${options.animationTime}`)
-                $images.eq(counter).one('animationend', function () {
-                    $images.eq(counter).removeClass(`animate__animated animate__fadeInDown ${options.animationTime}`)
-                    isRun = false
-                })
-            })
-        }
-
-        function prevSlideFadeDownFadeUp() {
-            $images.eq(counter).addClass(`animate__animated animate__fadeOutUp ${options.animationTime}`)
-
-            $images.eq(counter).one('animationend', function () {
-                $images.eq(counter).removeClass(`animate__animated animate__fadeOutUp ${options.animationTime}`)
-                hideCurrentSlide()
-                counterDec()
-                showCurrentSlide()
-                if (options.constSliderHeight === false) {
-                    resizeContainer()
-                }
-                $images.eq(counter).addClass(`animate__animated animate__fadeInUp ${options.animationTime}`)
-                $images.eq(counter).one('animationend', function () {
-                    $images.eq(counter).removeClass(`animate__animated animate__fadeInUp ${options.animationTime}`)
-                    isRun = false
-                })
-            })
-        }
-
 
 
         return this
--- js/app.js
+++ js/app.js
@@ -4,18 +4,229 @@ $(function () {
 
     $('#galery').animateCssSlider({}, [])
 
+    $('#galery1').animateCssSlider({
+        auto: true,
+        autoplayInterval: 7500,
+        animationType: 'fadeOutInDown/fadeOutInUp',
+        animationTime: 'animate__slower',
+        constSliderHeight: false,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/551/550', '#'],
+        ['https://picsum.photos/552/550', '#'],
+        ['https://picsum.photos/553/550', '#'],
+        ['https://picsum.photos/554/550', '#'],
+        ['https://picsum.photos/555/550', '#'],
+    ])
+
     $('#galery2').animateCssSlider({
         auto: true,
-        autoplayInterval: 3000,
-        animationType: 'fadeDown/fadeUp',
+        autoplayInterval: 4000,
+        animationType: 'fadeOutInDownBig/fadeOutInUpBig',
         animationTime: 'animate__fast',
         constSliderHeight: false,
+        overflowHidden: true,
     }, [
-        ['https://picsum.photos/651', '#'],
-        ['https://picsum.photos/652', '#'],
-        ['https://picsum.photos/653', '#'],
-        ['https://picsum.photos/654', '#'],
-        ['https://picsum.photos/655', '#'],
+        ['https://picsum.photos/556/550', '#'],
+        ['https://picsum.photos/557/550', '#'],
+        ['https://picsum.photos/558/550', '#'],
+        ['https://picsum.photos/559/550', '#'],
+        ['https://picsum.photos/560/550', '#'],
+    ])
+
+    $('#galery3').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4000,
+        animationType: 'fadeOutInLeft/fadeOutInRight',
+        animationTime: 'animate__faster',
+        constSliderHeight: false,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/561/550', '#'],
+        ['https://picsum.photos/562/550', '#'],
+        ['https://picsum.photos/563/550', '#'],
+        ['https://picsum.photos/564/550', '#'],
+        ['https://picsum.photos/565/550', '#'],
+    ])
+
+    $('#galery4').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4500,
+        animationType: 'fadeOutInLeftBig/fadeOutInRightBig',
+        animationTime: 'animate__faster',
+        constSliderHeight: false,
+        overflowHidden: false,
+    }, [
+        ['https://picsum.photos/566/550', '#'],
+        ['https://picsum.photos/567/550', '#'],
+        ['https://picsum.photos/568/550', '#'],
+        ['https://picsum.photos/569/550', '#'],
+        ['https://picsum.photos/570/550', '#'],
+    ])
+
+    $('#galery5').animateCssSlider({
+        auto: true,
+        autoplayInterval: 5500,
+        animationType: 'fadeOutInLeftTop/fadeOutInRightBottom',
+        animationTime: 'animate__slow',
+        constSliderHeight: false,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/571/550', '#'],
+        ['https://picsum.photos/572/550', '#'],
+        ['https://picsum.photos/573/550', '#'],
+        ['https://picsum.photos/574/550', '#'],
+        ['https://picsum.photos/575/550', '#'],
+    ])
+
+    $('#galery6').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4000,
+        animationType: 'fadeOutInLeftBottom/fadeOutInRightTop',
+        animationTime: '',
+        constSliderHeight: false,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/576/550', '#'],
+        ['https://picsum.photos/577/550', '#'],
+        ['https://picsum.photos/578/550', '#'],
+        ['https://picsum.photos/579/550', '#'],
+        ['https://picsum.photos/580/550', '#'],
+    ])
+
+    $('#galery7').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4500,
+        animationType: 'backOutInDown/backOutInUp',
+        animationTime: '',
+        constSliderHeight: true,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/550', '#'],
+        ['https://picsum.photos/551', '#'],
+        ['https://picsum.photos/552', '#'],
+        ['https://picsum.photos/553', '#'],
+        ['https://picsum.photos/554', '#'],
+    ])
+
+    $('#galery8').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4500,
+        animationType: 'backOutInLeft/backOutInRight',
+        animationTime: '',
+        constSliderHeight: true,
+        overflowHidden: false,
+    }, [
+        ['https://picsum.photos/555/550', '#'],
+        ['https://picsum.photos/506/450', '#'],
+        ['https://picsum.photos/557/550', '#'],
+        ['https://picsum.photos/518/450', '#'],
+        ['https://picsum.photos/519/550', '#'],
+    ])
+
+    $('#galery9').animateCssSlider({
+        auto: true,
+        autoplayInterval: 5500,
+        animationType: 'bounceOut/bounceIn',
+        animationTime: 'animate__slow',
+        constSliderHeight: false,
+        overflowHidden: false,
+    }, [
+        ['https://picsum.photos/550/550', '#'],
+        ['https://picsum.photos/551/550', '#'],
+        ['https://picsum.photos/552/550', '#'],
+        ['https://picsum.photos/553/550', '#'],
+        ['https://picsum.photos/554/550', '#'],
+    ])
+
+    $('#galery10').animateCssSlider({
+        auto: true,
+        autoplayInterval: 4500,
+        animationType: 'bounceOutInDown/bounceOutInUp',
+        animationTime: '',
+        constSliderHeight: false,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/555/550', '#'],
+        ['https://picsum.photos/556/550', '#'],
+        ['https://picsum.photos/557/550', '#'],
+        ['https://picsum.photos/558/550', '#'],
+        ['https://picsum.photos/559/550', '#'],
+    ])
+
+    $('#galery11').animateCssSlider({
+        auto: true,
+        autoplayInterval: 5500,
+        animationType: 'bounceOutInLeft/bounceOutInRight',
+        animationTime: 'animate__slow',
+        constSliderHeight: false,
+        overflowHidden: false,
+    }, [
+        ['https://picsum.photos/560/550', '#'],
+        ['https://picsum.photos/561/550', '#'],
+        ['https://picsum.photos/562/550', '#'],
+        ['https://picsum.photos/563/550', '#'],
+        ['https://picsum.photos/564/550', '#'],
+    ])
+
+    $('#galery12').animateCssSlider({
+        auto: true,
+        autoplayInterval: 6000,
+        animationType: 'flipOutX/flipInX',
+        animationTime: 'animate__slow',
+        constSliderHeight: true,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/565/550', '#'],
+        ['https://picsum.photos/566/550', '#'],
+        ['https://picsum.photos/567/550', '#'],
+        ['https://picsum.photos/568/550', '#'],
+        ['https://picsum.photos/569/550', '#'],
+    ])
+
+    $('#galery13').animateCssSlider({
+        auto: true,
+        autoplayInterval: 6500,
+        animationType: 'flipOutY/flipInY',
+        animationTime: 'animate__slow',
+        constSliderHeight: true,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/550/550', '#'],
+        ['https://picsum.photos/551/550', '#'],
+        ['https://picsum.photos/552/550', '#'],
+        ['https://picsum.photos/553/550', '#'],
+        ['https://picsum.photos/554/550', '#'],
+    ])
+
+    $('#galery14').animateCssSlider({
+        auto: true,
+        autoplayInterval: 5500,
+        animationType: 'lightSpeedOutInLeft/lightSpeedOutInRight',
+        animationTime: '',
+        constSliderHeight: false,
+        overflowHidden: false,
+    }, [
+        ['https://picsum.photos/555/550', '#'],
+        ['https://picsum.photos/556/550', '#'],
+        ['https://picsum.photos/557/550', '#'],
+        ['https://picsum.photos/558/550', '#'],
+        ['https://picsum.photos/559/550', '#'],
+    ])
+
+    $('#galery15').animateCssSlider({
+        auto: true,
+        autoplayInterval: 5500,
+        animationType: 'rotateOut/rotateIn',
+        animationTime: '',
+        constSliderHeight: true,
+        overflowHidden: true,
+    }, [
+        ['https://picsum.photos/560/550', '#'],
+        ['https://picsum.photos/561/550', '#'],
+        ['https://picsum.photos/562/550', '#'],
+        ['https://picsum.photos/563/550', '#'],
+        ['https://picsum.photos/564/550', '#'],
     ])
 
 })
